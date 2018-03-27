8a217abfae7408ab38a9cf2c7aa813b6f3d1f57a
Dustin Deus <deusdustin@gmail.com>
2018-03-27 17:24:29 +0200

    fix test timeout

--- plugin.test.js
+++ plugin.test.js
@@ -6,34 +6,40 @@ const ts = require('hemera-testsuite')
 const hp = require('hemera-plugin')
 const build = require('./example')
 
-const port = 4222
+const port = 4250
+let server = null
+
+test('setup', t => {
+  server = ts.start_server(port, err => {
+    t.error(err)
+    t.end()
+  })
+})
 
 test('Should be able to register a plugin', t => {
   t.plan(3)
 
-  const server = ts.start_server(port, err => {
+  const fastify = build({
+    plugins: [
+      hp(
+        function myPlugin(hemera, options, done) {
+          t.deepEqual(options, { a: 1 })
+          done()
+        },
+        {
+          name: 'myPlugin',
+          options: { a: 1 }
+        }
+      )
+    ],
+    nats: 'nats://127.0.0.1:' + port
+  })
+
+  fastify.ready(err => {
     t.error(err)
-    const fastify = build({
-      plugins: [
-        hp(
-          function myPlugin(hemera, options, done) {
-            t.deepEqual(options, { a: 1 })
-            done()
-          },
-          {
-            name: 'myPlugin',
-            options: { a: 1 }
-          }
-        )
-      ],
-      nats: 'nats://127.0.0.1:' + port
-    })
-    fastify.ready(() => {
+    fastify.close(err => {
       t.error(err)
-      fastify.close(() => {
-        server.kill()
-        t.end()
-      })
+      t.end()
     })
   })
 })
@@ -41,34 +47,37 @@ test('Should be able to register a plugin', t => {
 test('Should be able to register a plugin with custom options', t => {
   t.plan(3)
 
-  const server = ts.start_server(port, err => {
-    t.error(err)
-    const fastify = build({
-      plugins: [
-        {
-          plugin: hp(
-            function myPlugin(hemera, options, done) {
-              t.deepEqual(options, { a: 1, b: 2 })
-              done()
-            },
-            {
-              name: 'myPlugin',
-              options: { a: 1 }
-            }
-          ),
-          options: {
-            b: 2
+  const fastify = build({
+    plugins: [
+      {
+        plugin: hp(
+          function myPlugin(hemera, options, done) {
+            t.deepEqual(options, { a: 1, b: 2 })
+            done()
+          },
+          {
+            name: 'myPlugin',
+            options: { a: 1 }
           }
+        ),
+        options: {
+          b: 2
         }
-      ],
-      nats: 'nats://127.0.0.1:' + port
-    })
-    fastify.ready(() => {
+      }
+    ],
+    nats: 'nats://127.0.0.1:' + port
+  })
+
+  fastify.ready(err => {
+    t.error(err)
+    fastify.close(err => {
       t.error(err)
-      fastify.close(() => {
-        server.kill()
-        t.end()
-      })
+      t.end()
     })
   })
+})
+
+test('teardown', function(t) {
+  server.kill()
+  t.end()
 })
